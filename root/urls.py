7213c4d0997bbce49667a6ed785b1a2fc9a3635d
MaryamKalantarii <137641133+MaryamKalantarii@users.noreply.github.com>
2023-08-27 22:47:23 +0430

    load

---
+++ root/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+app_name = 'root'
+
+urlpatterns = [
+    path("",home,name="home"),
+    path("about",about,name="about"),
+    # path("contact",contact,name="contact"),
+    # path("trainer",trainer,name="trainer"),
+    # path("course-detail/<int:id>",course_detail,name="course_detail"),
+]
